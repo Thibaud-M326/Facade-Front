d95757abfbd1d4c8212ac7c05525346e290badac
Thibaud-M326 <thibaudmaitre@gmail.com>
2023-11-20 19:23:46 +0100

    route gard added for /userAccount and /cart

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,7 +7,8 @@ const router = createRouter({
     {
       path: '/new',
       name: 'new',
-      component: NewView
+      component: NewView,
+      meta: { requiresAuth: false },
     },
     {
       path: '/men',
@@ -15,39 +16,60 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/MenView.vue')
+      component: () => import('../views/MenView.vue'),
+      meta: { requiresAuth: false },
     },
     {
       path: '/women',
       name: 'women',
-      component: () => import('../views/WomenView.vue')
+      component: () => import('../views/WomenView.vue'),
+      meta: { requiresAuth: false },
     },
     {
       path: '/createAccount',
       name: 'createAccount',
-      component: () => import('../views/CreateAccountView.vue')
+      component: () => import('../views/CreateAccountView.vue'),
+      meta: { requiresAuth: false },
     },
     {
       path: '/userAccount',
       name: 'userAccount',
-      component: () => import('../views/UserAccountView.vue')
+      component: () => import('../views/UserAccountView.vue'),
+      meta: { requiresAuth: true },
     },
     {
       path: '/cart',
       name: 'cart',
-      component: () => import('../views/CartView.vue')
+      component: () => import('../views/CartView.vue'),
+      meta: { requiresAuth: true },
     },
     {
       path: '/product/:productId',
       name: 'product',
       component: () => import('../views/ProductView.vue'),
+      meta: { requiresAuth: false },
     },
     {
       path: '/test',
       name: 'testProps',
       component: () => import('../views/testPropsView.vue'),
+      meta: { requiresAuth: false },
     },
   ]
+})
+
+router.beforeEach((to, from, next) => {
+
+  if(to.meta.requiresAuth && !localStorage.getItem("apollo-token")) {
+    next ({
+      path: '/new',
+      name: 'new'
+    })
+  }
+  else {
+    next()
+  }
+   
 })
 
 export default router
